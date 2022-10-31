4955a4d921a5cac2327b553bb31c867ffb019214
Edy <2011500039@mahasiswa.atmaluhur.ac.id>
2022-10-31 18:47:40 +0700

    RecyleCardView

---
+++ app/src/main/java/id/ac/atmaluhur/recyclecardview/Prodi.kt
@@ -0,0 +1,10 @@
+package id.ac.atmaluhur.recyclecardview
+
+class Prodi {
+
+    val warna: String,
+    val jenjang: String,
+    val nmProdi: String,
+    val fakultas: String,
+    val konsentrasi: Array<String> = arrayOf(nmProdi)
+}
